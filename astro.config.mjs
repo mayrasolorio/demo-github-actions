7ef547ab73997c6ff491218a1f2808c271e055a9
mayrasolorio <mayrasolorio03@gmail.com>
2025-05-08 23:27:47 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
-  site: "https://adulbrich.github.io",
+  site: "https://mayrasolorio.github.io",
   base: "/demo-github-actions",
 });
